ac161f0276b6aeb03802b230911fb0c508a2934f
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:53:56 +0530

    initial commit

---
+++ src/MockData/profile.js
@@ -0,0 +1,36 @@
+export const profiles = [
+    
+    
+    {
+        id: 1,
+        name: 'Samurah Keith Pasala',
+        nickName: "Duggu",
+        dob: "01/23/2023",
+        state: "Houston",
+        time: "3:01pm",
+        place: "Woodlands",
+        Hospital: "Memorial Herman",
+        deliveredDoctor: "DR.Steven Strong",
+        birthWeight: "7lbs",
+        firstPediatrician: "DR.Neha Manikonda",
+        secondPediatrician: "Dr.Justin hygen",
+        presentAge: "3 and a half years old",
+    },
+  
+    {
+        id: 2,
+        name: 'Shammah Kemmuel Pasala',
+        nickName: "Mikku",
+        dob: "02/27/2025",
+        state: "St Louis",
+        time: "5:41pm",
+        place: "Wentzville",
+        Hospital: "St Joseph's Hospital",
+        deliveredDoctor: "DR.Emma Eubanks",
+        birthWeight: "6lbs",
+        prediatrician: "Dr.Justin Thomas hagen",
+        presentAge: "1 and a half years old",
+    }
+
+
+]
