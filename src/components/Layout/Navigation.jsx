409ff84f3d1243dc47d34cf8beabafa1b76c6258
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:43:14 +0530

    initial commit

---
+++ src/components/Layout/Navigation.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router';
+
+const Navigation = () => {
+    return(
+        <nav>
+            <ul className="navigation">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/childrenprofile">Children Profile</Link>
+                </li>
+                <li>
+                    <Link to="/memories">Memories</Link>
+                </li>
+                <li>
+                    <Link to="/milestones">Milestones</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">Gallery</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+export default Navigation;
