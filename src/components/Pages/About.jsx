c2ce75d7e23da7e1cae44feb4920196e68473810
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:54:32 +0530

    initial commit

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,38 @@
+const AboutPage =() => {
+
+    return(
+        <>
+        <div className="about">
+            <h1> About US </h1>
+            <p>
+                Kirthi and her husband have two boys, making them a family of four. 
+                Kirthi has always wanted to store the core memories of her sons, a passion 
+                she frequently discusses with her husband, who shares the exact same interest. 
+                She wants an app where she can record the most important information about her 
+                sons,including their medical data, milestones, and the specific dates when her 
+                babies reached them.The app should also hold pictures from the day they were born 
+                until the present,featuring "add" and "save" buttons to seamlessly include more 
+                memories as they grow.She intends to document all of their "firsts"—such as their 
+                first road trip, first flight,and first beach visit—in an app built for the long run. 
+                Once the boys turn 21, she wants to convert these digital entries into a PDF or a 
+                physical memory book to gift to her sons.			
+            </p>
+
+            <br></br>
+            <p>
+                Sagar, the father of two boys and husband of Kirthi, is bad at remembering the dates of his sons' 
+                milestones and their "firsts." Because Kirthi remembers every single date of their memories, Sagar 
+                wants her to build this memory app. He can then use the app to look up special dates and see when 
+                their "firsts" happened.Essentially, he wants to go through the app and cherish those memories with 
+                Kirthi.It was actually Sagar's idea to combine this digital memory book with physical keepsakes—such 
+                as their umbilical cords, hospital bands, and visitor tags—to gift to their sons on their 21st birthday.
+            </p>
+
+            <img src="public/home.jpeg" alt="family picture" height="300"/>
+        
+        </div>
+        </>
+    )
+
+}
+export default AboutPage;
