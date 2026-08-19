8acdfa33db1fb705c17960d4bdae9a245152d9ae
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:42:33 +0530

    initial commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./Navigation";
+
+const Header = () => {
+    return(
+        <header>
+            <div className="header">
+                <h1>CHILDRENS MEMORY LANE</h1>
+            </div>
+            <Navigation/>
+        </header>
+    );
+};
+
+export default Header;
