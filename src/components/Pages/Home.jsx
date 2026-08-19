3cb21bcce034594f468d6f95afa6bb16e713e606
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:54:16 +0530

    initial commit

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,18 @@
+const HomePage = () => {
+
+    return(
+        <> 
+        <div className="home">
+            <h1>Welcome!</h1>
+            <p>
+                ❤️👦 Every Child is Precious For Parents 👦❤️
+            </p>
+        </div>
+
+        <img src="family.jpg" alt="family picture" height="300"/>
+        
+        </>
+    );
+};
+
+export default HomePage;
