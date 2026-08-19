21e5d589b9451f0663218ef44a8cc6ae10cd65d5
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-19 23:54:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, LaunchCode</h1>
-    </div>
+import { Routes, Route } from 'react-router';
+import Header from './components/Layout/Header';
+import HomePage from './components/Pages/Home';
+import AboutPage from './components/Pages/About';
+import ProfilePage from './components/Pages/ChildrenProfile';
+import MemoryCard from './components/Pages/Memories';
+import MilestonePage from './components/Pages/Milestones';
+import Gallery from './components/Pages/Gallery';
+import Footer from './components/Layout/Footer';
+import './app.css';
+
+const App = () => {
+  return(
+    <>
+    <Header/>
+    <Routes>
+      
+      
+      
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/childrenprofile" element={<ProfilePage/>}/>
+      <Route path="/memories" element={<MemoryCard/>}/>
+      <Route path="/milestones" element={<MilestonePage/>}/>
+      <Route path="/gallery" element={<Gallery/>}/>
+
+
+      
+
+
+    </Routes>
+    <Footer/>
+    
+    </>
   );
-}
+};
 
 export default App;
+
