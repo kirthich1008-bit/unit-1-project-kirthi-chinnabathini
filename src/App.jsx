bcef53e34e44943e05cb7b4ca43b0e130bcf12cb
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-25 01:01:58 +0530

    completed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { Routes, Route } from 'react-router';
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import HomePage from './components/Pages/Home';
 import AboutPage from './components/Pages/About';
 import ProfilePage from './components/Pages/ChildrenProfile';
-import MemoryCard from './components/Pages/Memories';
+import MemoryPage from './components/Pages/Memories';
 import MilestonePage from './components/Pages/Milestones';
-import Gallery from './components/Pages/Gallery';
+import GalleryPage from './components/Pages/Gallery';
 import Footer from './components/Layout/Footer';
 import './app.css';
 
 const App = () => {
+   
+  const [galleryData] = useState(() => {
+    const parse = (key) => JSON.parse(localStorage.getItem(key)) || { Duggu: [], Mikku: [] };
+    const milestones = parse('childrenMilestones');
+    const memories = parse('childrenMemories');
+    const combined = [];
+
+    // merging data to gallery page
+    const mergeData = (dataObject, type) => {
+      Object.entries(dataObject).forEach(([childName, data]) => {
+        if (Array.isArray(data)) {
+          data.forEach((item) => combined.push({ ...item, childName, type }));
+        }
+      });
+    };
+
+    mergeData(milestones, 'Milestone');
+    mergeData(memories, 'Memory');
+
+    return combined.sort((a, b) => new Date(b.date) - new Date(a.date));
+  });
+
   return(
     <>
     <Header/>
@@ -20,9 +43,9 @@ const App = () => {
       <Route path="/" element={<HomePage/>} />
       <Route path="/about" element={<AboutPage/>}/>
       <Route path="/childrenprofile" element={<ProfilePage/>}/>
-      <Route path="/memories" element={<MemoryCard/>}/>
+      <Route path="/memories" element={<MemoryPage/>}/>
       <Route path="/milestones" element={<MilestonePage/>}/>
-      <Route path="/gallery" element={<Gallery/>}/>
+      <Route path="/gallery" element={<GalleryPage data={galleryData}/>}/>
 
 
       
