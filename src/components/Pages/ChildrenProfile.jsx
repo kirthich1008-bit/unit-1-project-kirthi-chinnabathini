a43220df524c4a989d6979b8a9c4e0a84d4cf4c1
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-27 02:34:20 +0530

    css file name changed.

--- src/components/Pages/ChildrenProfile.jsx
+++ src/components/Pages/ChildrenProfile.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router';
 import { profiles } from '../../MockData/profile';
+import './Pages.css'
 
 
 function ProfilePage() {
