e0a051addfbd40470c5f748b35384112062e3133
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-29 04:30:27 +0530

    made a slight change for file location.

---
+++ src/components/MockData/profile.js
@@ -0,0 +1,40 @@
+
+
+export const profiles = [
+    
+    
+    {
+        id: 1,
+        name: 'Samurah Keith Pasala',
+        nickName: "Duggu",
+        dob: "01/23/2023",
+        state: "Houston",
+        time: "3:01pm",
+        place: "Woodlands",
+        Hospital: "Memorial Herman",
+        deliveredDoctor: "DR.Steven Strong",
+        birthWeight: "7lbs",
+        pediatrician: "Dr.Justin hygen",
+        presentAge: "3 and a half years old",
+        homeAddress: "2345 Sawdust Road APT 2333 Woodlands-TX-22830"
+                     
+    },
+  
+    {
+        id: 2,
+        name: 'Shammah Kemmuel Pasala',
+        nickName: "Mikku",
+        dob: "02/27/2025",
+        state: "St Louis",
+        time: "5:41pm",
+        place: "Wentzville",
+        Hospital: "St Joseph's Hospital",
+        deliveredDoctor: "DR.Emma Eubanks",
+        birthWeight: "6lbs",
+        pediatrician: "Dr.Justin hygen",
+        presentAge: "1 and a half years old",
+        homeAddress: "436 Compassion CIR Wentzville MO-63385-2407"
+    }
+
+
+]
