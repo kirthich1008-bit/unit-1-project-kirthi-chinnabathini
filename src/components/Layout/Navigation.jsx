b31e5878c5517f4ab31428cc1f3e546f7604d7bc
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-08-29 04:29:57 +0530

    added new feature.

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -23,6 +23,9 @@ const Navigation = () => {
                 <li>
                     <Link to="/gallery">Gallery</Link>
                 </li>
+                <li>
+                    <Link to="/letters">Letters</Link>
+                </li>
             </ul>
         </nav>
     );
